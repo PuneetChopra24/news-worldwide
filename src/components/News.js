b72bae40f07f2df8c2737888531b26edd413497f
Puneet Chopra <pchopra0250@conestogac.on.ca>
2022-03-14 00:31:05 -0400

    refactored News component

--- src/components/News.js
+++ src/components/News.js
@@ -26,10 +26,9 @@ export class News extends Component {
     };
   }
 
-  async componentDidMount() {
-    console.log("cdm");
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=038ddfdac8e64511b2f8c95dfc128e2d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=038ddfdac8e64511b2f8c95dfc128e2d&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
@@ -40,53 +39,18 @@ export class News extends Component {
     });
   }
 
-  handleNextClick = async () => {
-    console.log("Next button clicked");
-    if (
-      !(
-        this.state.page + 1 >
-        Math.ceil(this.state.totalResults / this.props.pageSize)
-      )
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&category=${
-        this.props.category
-      }&apiKey=038ddfdac8e64511b2f8c95dfc128e2d&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      console.log(parsedData);
-      this.setState({ articles: parsedData.articles });
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading: false,
-      });
-    }
-  };
+  async componentDidMount() {
+    this.updateNews();
+  }
 
   handlePrevClick = async () => {
-    console.log("Previous button clicked.");
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${
-      this.props.category
-    }&apiKey=038ddfdac8e64511b2f8c95dfc128e2d&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({ articles: parsedData.articles });
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false,
-    });
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
+  };
+
+  handleNextClick = async () => {
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
   };
 
   render() {
@@ -108,6 +72,9 @@ export class News extends Component {
                     }
                     imageUrl={element.urlToImage}
                     newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
                   />
                 </div>
               );
